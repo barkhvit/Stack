03f1a40bce30397892a232ac59e35d4b424c74c4
barkh <barkh@LAPTOP-37335FPD>
2025-03-20 19:49:55 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input=="exit") return;
+            }
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    internal class Stack
+    {
+        public List<string> stack = new List<string>();
+
+        public string Top { get
+            {
+                if (stack.Count == 0)
+                    return null;
+
+                return stack[stack.Count - 1];
+
+            } }
+        public Stack(params string[] parameters)
+        {
+            foreach (var parameter in parameters)
+            {
+                Add(parameter);
+            }
+        }
+        public void Add(string name)
+        {
+            stack.Add(name);
+        }
+
+        public void Print()
+        {
+            foreach (var parameter in stack) 
+                Console.WriteLine(parameter.ToString());
+        }
+
+        
+
+    }
+}
